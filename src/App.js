27bf75debc507119061cb51cff72950b3e1a0000
nasarabu <abunasar388@gmail.com>
2019-09-02 01:31:29 +0530

    created some functionality in sign-in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import { Switch,Route } from 'react-router-dom';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 const HatsPage = () =>(
   <div>
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component = {ShopPage}/>
+        <Route exact path='/signin' component = {SignInAndSignUp} />
       </Switch>
     </div>
   );
